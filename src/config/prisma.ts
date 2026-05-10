f53962892c2dddb457fe1344ee0d2f091ab2a7b9
Ntunga08 <samwelbuluba22@gmail.com>
2026-05-10 08:56:59 +0300

    Update Prisma configuration with connection pool

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,6 +1,18 @@
 import "dotenv/config";
+import { Pool } from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
+
+// console.log("PRISMA DATABASE_URL:", process.env.DATABASE_URL);
+// console.log("TYPE:", typeof process.env.DATABASE_URL);
+
+//const parsed = new URL(process.env.DATABASE_URL!);
+
+// console.log("HOST:", parsed.hostname);
+// console.log("PORT:", parsed.port);
+// console.log("USER:", parsed.username);
+// console.log("PASSWORD:", parsed.password);
+// console.log("DB:", parsed.pathname);
 
 const connectionString = process.env.DATABASE_URL;
 
@@ -8,13 +20,19 @@ if (!connectionString) {
   throw new Error("DATABASE_URL is not set");
 }
 
-const adapter = new PrismaPg(connectionString);
+const pool = new Pool({
+  connectionString,
+});
+
+const adapter = new PrismaPg(pool);
 
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
-const prisma = globalThis.prisma ?? new PrismaClient({ adapter });
+const prisma = globalThis.prisma ?? new PrismaClient({
+  adapter,
+});
 
 if (process.env.NODE_ENV !== "production") {
   globalThis.prisma = prisma;
